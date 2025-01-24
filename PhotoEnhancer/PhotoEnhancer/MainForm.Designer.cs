b73a7a0bc68eb242d06037f4d7d147c4a84730d0
ProstoGhost <tam_1910@bk.ru>
2025-01-25 00:34:55 +0500

    Добавлены кнопки: сохранить, загрузить, применить с заменой. А так же реализована функция Drun'n'Drop. Поддерживаемые форматы фото - jpg, jpeg, tiff

--- PhotoEnhancer/PhotoEnhancer/MainForm.Designer.cs
+++ PhotoEnhancer/PhotoEnhancer/MainForm.Designer.cs
@@ -28,21 +28,26 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
             this.filtersComboBox = new System.Windows.Forms.ComboBox();
             this.applyButton = new System.Windows.Forms.Button();
             this.originalPictureBox = new System.Windows.Forms.PictureBox();
             this.resultPictureBox = new System.Windows.Forms.PictureBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.replaceButton = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.originalPictureBox)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.resultPictureBox)).BeginInit();
             this.SuspendLayout();
             // 
             // filtersComboBox
             // 
+            this.filtersComboBox.BackColor = System.Drawing.SystemColors.HighlightText;
             this.filtersComboBox.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
             this.filtersComboBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
             this.filtersComboBox.FormattingEnabled = true;
             this.filtersComboBox.Location = new System.Drawing.Point(16, 16);
-            this.filtersComboBox.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.filtersComboBox.Margin = new System.Windows.Forms.Padding(4);
             this.filtersComboBox.Name = "filtersComboBox";
             this.filtersComboBox.Size = new System.Drawing.Size(342, 28);
             this.filtersComboBox.TabIndex = 0;
@@ -50,13 +55,15 @@
             // 
             // applyButton
             // 
-            this.applyButton.Location = new System.Drawing.Point(13, 606);
-            this.applyButton.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.applyButton.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.applyButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.applyButton.Location = new System.Drawing.Point(13, 558);
+            this.applyButton.Margin = new System.Windows.Forms.Padding(4);
             this.applyButton.Name = "applyButton";
-            this.applyButton.Size = new System.Drawing.Size(130, 40);
+            this.applyButton.Size = new System.Drawing.Size(235, 40);
             this.applyButton.TabIndex = 1;
             this.applyButton.Text = "Применить";
-            this.applyButton.UseVisualStyleBackColor = true;
+            this.applyButton.UseVisualStyleBackColor = false;
             this.applyButton.Visible = false;
             this.applyButton.Click += new System.EventHandler(this.applyButton_Click);
             // 
@@ -78,18 +85,62 @@
             this.resultPictureBox.TabIndex = 2;
             this.resultPictureBox.TabStop = false;
             // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Location = new System.Drawing.Point(255, 558);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(112, 39);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Открыть";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button2.Location = new System.Drawing.Point(255, 607);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(112, 40);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "Сохранить";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // replaceButton
+            // 
+            this.replaceButton.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.replaceButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.replaceButton.Location = new System.Drawing.Point(13, 606);
+            this.replaceButton.Margin = new System.Windows.Forms.Padding(4);
+            this.replaceButton.Name = "replaceButton";
+            this.replaceButton.Size = new System.Drawing.Size(235, 40);
+            this.replaceButton.TabIndex = 5;
+            this.replaceButton.Text = "Применить с заменой";
+            this.replaceButton.UseVisualStyleBackColor = false;
+            this.replaceButton.Visible = false;
+            this.replaceButton.Click += new System.EventHandler(this.replaceButton_Click);
+            // 
             // MainForm
             // 
+            this.AllowDrop = true;
             this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Window;
             this.ClientSize = new System.Drawing.Size(808, 659);
+            this.Controls.Add(this.replaceButton);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
             this.Controls.Add(this.resultPictureBox);
             this.Controls.Add(this.originalPictureBox);
             this.Controls.Add(this.applyButton);
             this.Controls.Add(this.filtersComboBox);
             this.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
-            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(4);
             this.MaximizeBox = false;
             this.Name = "MainForm";
             this.Text = "PhotoEnhancer";
@@ -105,6 +156,9 @@
         private System.Windows.Forms.Button applyButton;
         private System.Windows.Forms.PictureBox originalPictureBox;
         private System.Windows.Forms.PictureBox resultPictureBox;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button replaceButton;
     }
 }
 
--- PhotoEnhancer/PhotoEnhancer/MainForm.cs
+++ PhotoEnhancer/PhotoEnhancer/MainForm.cs
@@ -7,11 +7,14 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.IO;
 
 namespace PhotoEnhancer
 {
     public partial class MainForm : Form
     {
+        private Label dragDropLabel; // Объявляем Label для отображения сообщения
+
         Panel parametersPanel;
         List<NumericUpDown> parameterControls;
 
@@ -22,14 +25,29 @@ namespace PhotoEnhancer
         {
             InitializeComponent();
 
-            var bmp = (Bitmap)Image.FromFile("cat.jpg");
-            originalPhoto = Convertors.BitmapToPhoto(bmp);
-            originalPictureBox.Image = bmp;
+            this.AllowDrop = true;
+            this.DragEnter += MainForm_DragEnter;
+            this.DragDrop += MainForm_DragDrop;
+            this.DragLeave += MainForm_DragLeave;
+
+            dragDropLabel = new Label
+            {
+                Text = "Перетащите изображение сюда",
+                Font = new Font("Play", 32),
+                ForeColor = Color.White,
+                BackColor = Color.FromArgb(150, 0, 0, 0), // Полупрозрачный черный фон
+                AutoSize = false,
+                TextAlign = ContentAlignment.MiddleCenter,
+                Visible = false,
+                Dock = DockStyle.Fill // Заполняем весь размер формы
+            };
+            this.Controls.Add(dragDropLabel);
         }
 
         private void filtersComboBox_SelectedIndexChanged(object sender, EventArgs e)
         {
             applyButton.Visible = true;
+            replaceButton.Visible = true;
 
             if(parametersPanel != null)
                 this.Controls.Remove(parametersPanel);
@@ -91,6 +109,7 @@ namespace PhotoEnhancer
 
             resultPhoto = filter.Process(originalPhoto, parameters);
             resultPictureBox.Image = Convertors.PhotoToBitmap(resultPhoto);
+            resultPictureBox.Visible = true;
         }
 
         public void AddFilter(IFilter filter)
@@ -98,6 +117,146 @@ namespace PhotoEnhancer
             if (filter == null) return;
 
             filtersComboBox.Items.Add(filter);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog
+            {
+                Filter = "Изображения|*.jpg;*.jpeg;*.tiff", // Фильтр для выбора изображений
+                Title = "Выберите изображение"
+            };
+            if (openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                // Получаем путь к выбранному файлу
+                string filePath = openFileDialog.FileName;
+
+                try
+                {
+                    // Загружаем изображение в объект Bitmap
+                    Bitmap bmp = (Bitmap)Image.FromFile(filePath);
+
+                    // Преобразуем Bitmap в Photo (если это необходимо)
+                    originalPhoto = Convertors.BitmapToPhoto(bmp);
+
+                    // Отображаем изображение в PictureBox
+                    originalPictureBox.Image = bmp;
+                }
+                catch(Exception ex) {
+                    MessageBox.Show($"Ошибка загрузки изображения: {ex.Message}", "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            // Проверяем, что обработанное изображение существует
+            if (resultPhoto == null)
+            {
+                MessageBox.Show("Обработанное изображение не найдено. Примените фильтр к изображению.", "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
+            // Создаем объект SaveFileDialog
+            SaveFileDialog saveFileDialog = new SaveFileDialog
+            {
+                Filter = "Изображения|*.jpg;*.jpeg;*.tiff", // Фильтр для сохранения изображений
+                Title = "Сохранить изображение"
+            };
+            // Показываем диалоговое окно и проверяем результат
+            if (saveFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                // Получаем путь для сохранения файла
+                string filePath = saveFileDialog.FileName;
+
+                try
+                {
+                    // Сохраняем обработанное изображение
+                    Bitmap resultBitmap = Convertors.PhotoToBitmap(resultPhoto);
+                    resultBitmap.Save(filePath);
+                    MessageBox.Show("Изображение успешно сохранено.", "Успех", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show($"Ошибка сохранения изображения: {ex.Message}", "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+        }
+        private void MainForm_DragEnter(object sender, DragEventArgs e)
+        {
+            // Проверяем, что перетаскиваемый объект является файлом изображения
+            if (e.Data.GetDataPresent(DataFormats.FileDrop))
+            {
+                string[] files = (string[])e.Data.GetData(DataFormats.FileDrop);
+                if (files.Length > 0 && IsImageFile(files[0]))
+                {
+                    e.Effect = DragDropEffects.Copy;
+                    dragDropLabel.Visible = true; // Показываем Label
+                    dragDropLabel.BringToFront(); // Перемещаем Label в самый верхний слой
+                }
+            }
+        }
+        private void MainForm_DragDrop(object sender, DragEventArgs e)
+        {
+            if (e.Data.GetDataPresent(DataFormats.FileDrop))
+            {
+                string[] files = (string[])e.Data.GetData(DataFormats.FileDrop);
+                if (files.Length > 0)
+                {
+                    string filePath = files[0];
+
+                    try
+                    {
+                        // Загружаем изображение в объект Bitmap
+                        Bitmap bmp = (Bitmap)Image.FromFile(filePath);
+
+                        // Преобразуем Bitmap в Photo (если это необходимо)
+                        originalPhoto = Convertors.BitmapToPhoto(bmp);
+
+                        // Отображаем изображение в PictureBox
+                        originalPictureBox.Image = bmp;
+                    }
+                    catch (Exception ex)
+                    {
+                        MessageBox.Show($"Ошибка загрузки изображения: {ex.Message}", "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    }
+                }
+            }
+            dragDropLabel.Visible = false; // Скрываем Label после завершения перетаскивания
+        }
+        private void MainForm_DragLeave(object sender, EventArgs e)
+        {
+            dragDropLabel.Visible = false; // Скрываем Label, если перетаскивание покинуло форму
+        }
+        private bool IsImageFile(string filePath)
+        {
+            // Проверяем расширение файла на соответствие изображениям
+            string[] validExtensions = { ".jpg", ".jpeg", ".tiff" };
+            string extension = Path.GetExtension(filePath).ToLower();
+            return validExtensions.Contains(extension);
+        }
+
+        private void replaceButton_Click(object sender, EventArgs e)
+        {
+            var filter = filtersComboBox.SelectedItem as IFilter;
+
+            if (filter == null) return;
+
+            var parameters = new double[parameterControls.Count];
+
+            for (int i = 0; i < parameters.Length; i++)
+                parameters[i] = (double)parameterControls[i].Value;
+
+            //resultPhoto = filter.Process(originalPhoto, parameters);
+            //resultPictureBox.Image = Convertors.PhotoToBitmap(resultPhoto);
+
+            originalPhoto = filter.Process(originalPhoto, parameters);
+            originalPictureBox.Image = Convertors.PhotoToBitmap(originalPhoto);
+
+            resultPhoto = originalPhoto;
+            resultPictureBox.Image = originalPictureBox.Image;
+            resultPictureBox.Visible = false;
+            
         }
     }
 }
